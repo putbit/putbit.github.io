1bcf075904702d2ad862337b0163bd9390fa07ad
stephentuso <tusodev@gmail.com>
2023-10-24 15:32:34 -0400

    update theme

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['./layouts/**/*.html', './content/**/*.md'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: colors.white,
+          dark: colors.black,
+        },
+        secondary: {
+          light: colors.gray[100],
+          dark: colors.gray[900],
+        },
+        'text-primary': {
+          light: colors.black,
+          dark: colors.white,
+        },
+        'text-secondary': {
+          light: colors.gray[800],
+          dark: colors.gray[200],
+        },
+        'text-tertiary': {
+          light: colors.gray[700],
+          dark: colors.gray[300],
+        },
+      },
+    },
+  },
+  plugins: [],
+}
